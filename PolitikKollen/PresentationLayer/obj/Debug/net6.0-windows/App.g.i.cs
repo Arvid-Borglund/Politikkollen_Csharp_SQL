6ce4a97a769fd12c9dc29e464bedcb8661289b37
jakob9m <74905095+jakob9m@users.noreply.github.com>
2023-10-24 19:52:57 +0200

    test

--- PolitikKollen/PresentationLayer/obj/Debug/net6.0-windows/App.g.i.cs
+++ PolitikKollen/PresentationLayer/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace PresentationLayer {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace PresentationLayer {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             PresentationLayer.App app = new PresentationLayer.App();
             app.InitializeComponent();
